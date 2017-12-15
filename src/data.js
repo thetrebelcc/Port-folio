dbd15bbddb05975190abe7db2ec9163ad09e291e
Fabian Anguiano <fabiananguiano@Fabians-MacBook.home>
2017-12-15 11:19:52 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,59 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "https://github.com/thetrebelcc"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "http://linkedin"
+        },
+        {
+            "icon": "icon-mail-alt",
+            "url": "fabiananguiano@gmail.com"
+        }
+    ],
+    "experiences": [
+        {}
+    ],
+    "name": "Fabian Anguiano",
+    "header": "Fabian Anguiano",
+    "cta": {
+        "label": "Get my resume",
+        "url": "https://docs.google.com/document/d/1DsuT8HVzFXc1xCzg6zmHKjexOOgUQsTGQW_M6jGX06w/edit?usp=sharing"
+    },
+    "testimonials": [
+        {
+            "__key": "050f1c7e-b8a1-465a-b265-9fab5f35c09b",
+            "quote": "Incredibly talented and hardworking. A super friendly guy who is a frequent communicator",
+            "name": "Person Name",
+            "title": "CEO at Company"
+        },
+        {
+            "__key": "749b9b68-203d-4c19-adb2-9ebdf1de6d85",
+            "quote": "Wow, thank you for this, you probably have the most intuitive explanation for this problem!",
+            "name": "Person Name"
+        }
+    ],
+    "title": "Fabian Anguiano",
+    "projects": [],
+    "email": "fabiananguiano@gmail.com",
+    "description": "I am currently attending a full stack web development bootcamp. ",
+    "footerTitle": "Web Developer "
+}
